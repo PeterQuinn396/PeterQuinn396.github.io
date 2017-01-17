f6639798ea943c87f21db808daa0f814dddb1e35
Peter Quinn <peter.quinn396@gmail.com>
2017-01-17 14:20:59 -0500

    added carousel

---
+++ js/navbars.js
@@ -0,0 +1,12 @@
+var mainNavbar ="<div class="navbar-main">
+	<div class="col-sm-2 navback">
+	 <nav>
+		<ul class="nav nav-pills nav-stacked">
+			<li role="presentation" class="nav nav-item" id="home"><a href="#index">Home</a></li>
+			<li role ="presentation" class="nav nav-item" id="bio"><a href="bio.html">Bio</a></li>
+			<li role="presentation" class="nav nav-item"><a href="projects.html">Projects</a></li>
+			<li role = "presentation" class="nav nav-item"><a href="travel.html">Travel</a></li>	 
+		</ul>
+	</nav>
+	</div>
+	</div>";
